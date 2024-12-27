370f77d7073ba09b265b7fec414ffb7814c18692
arjun pangeni <eemc365@gmail.com>
2024-12-27 23:59:37 +0545

    calender update

--- components/ui/calendar.jsx
+++ components/ui/calendar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const calendar = () => {
-  return <div></div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+      placeat molestias quo, voluptates odio itaque sapiente expedita, obcaecati
+      soluta tempore, suscipit quibusdam laboriosam illum corporis magni
+      reiciendis illo? Ipsam, maiores officiis. Tempora in possimus excepturi
+      culpa labore porro, minima eius?
+    </div>
+  );
 };
 
 export default calendar;
